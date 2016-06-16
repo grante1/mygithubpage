e6dd1a3c02f2d9acf25778ab04d11832a4bb81eb
grante1 <grante1@spu.edu>
2016-06-15 16:15:41 -0800

    New UI
    Changed to MDB for the website.

---
+++ js/scrolltop.js
@@ -0,0 +1,34 @@
+// Get nav bar anchors to scroll down when clicked on
+
+$(document).ready(function(){
+    $(".nav-link").click(function(){
+        var name = $(this).attr("name");
+        if(name === 'inseattle')
+        {
+            $('html, body').animate({
+                scrollTop: $('#in-seattle').offset().top-90
+            }, 1500);
+        }
+        else if(name === 'oncampus')
+        {
+            $('html, body').animate({
+                scrollTop: $('#on-campus').offset().top-90
+            }, 1500);
+        }
+  });
+
+  $(".btn-circle").click(function(){
+      var name = $(this).attr("name");
+      if(name === 'learnMore')
+      {
+          $('html, body').animate({
+              scrollTop: $('#About').offset().top-90
+          }, 1500);
+      }
+});
+
+
+
+
+
+});
